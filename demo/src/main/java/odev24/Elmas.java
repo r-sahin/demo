5461f36cf217200a7b27a3b1c175a1859f5353c4
Ramazan <r.sahihny@gmail.com>
2026-04-24 18:23:16 +0300

    24. odev yapıldı

---
+++ demo/src/main/java/odev24/Elmas.java
@@ -0,0 +1,32 @@
+package odev24;
+
+import java.util.Scanner;
+
+public class Elmas {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz : ");
+        int n = klavye.nextInt();
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
