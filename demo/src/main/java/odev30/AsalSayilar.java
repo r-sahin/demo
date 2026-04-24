d8bc2a5c23b9d730e825d32f6097a1a333d6c75e
Ramazan <r.sahihny@gmail.com>
2026-04-24 18:33:42 +0300

    30. odev yapıldı

---
+++ demo/src/main/java/odev30/AsalSayilar.java
@@ -0,0 +1,19 @@
+package odev30;
+
+public class AsalSayilar {
+    public static void main(String[] args) {
+        for (int sayi = 2; sayi <= 100; sayi++) {
+            boolean asal = true;
+            for (int i = 2; i <= Math.sqrt(sayi); i++) {
+                if (sayi % i == 0) {
+                    asal = false;
+                    break;
+                }
+            }
+            if (asal) {
+                System.out.print(sayi + " ");
+            }
+        }
+
+    }
+}
